2c77953034ade314b3ffb6a09152becb3d773995
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-03-28 19:33:37 +0300

    controllers

---
+++ controllers/AuthorController.ts
@@ -0,0 +1,64 @@
+import { Request, Response} from "express";
+import mongoose from "mongoose";
+import AuthorModel from "models/AuthorModel";
+
+const create = (req: Request, res: Response) => {
+    const {name} = req.body;
+
+    const author = new AuthorModel({
+        _id: new mongoose.Types.ObjectId(),
+        name
+    })
+
+    return author
+            .save()
+            .then((author) => res.status(201).json({author}))
+            .catch((error) => res.status(500).json({error}))
+}
+
+const readById = (req: Request, res: Response) => {
+    const id = req.params.authorId;
+
+    return AuthorModel
+            .findById(id)
+            .then((author) => (author ? res.status(200).json({ author }) : res.status(404).json({ message: 'not found' })))
+            .catch((error) => res.status(500).json({ error }));
+}
+
+const readAll = (req: Request, res: Response) => {
+    return AuthorModel.find()
+        .then((authors) => res.status(200).json({ authors }))
+        .catch((error) => res.status(500).json({ error }));
+}
+
+const update = (req: Request, res: Response) => {
+    const id = req.params.authorId;
+
+    return AuthorModel
+            .findById(id)
+            .then((author) => {
+            if (author) {
+                author.set(req.body);
+
+                return author
+                    .save()
+                    .then((author) => res.status(201).json({ author }))
+                    .catch((error) => res.status(500).json({ error }));
+            } else {
+                return res.status(404).json({ message: 'not found' });
+            }
+        })
+        .catch((error) => res.status(500).json({ error }));
+}
+
+const deleteById = (req: Request, res: Response) => {
+    const id = req.params.authorId;
+
+    return AuthorModel
+            .findByIdAndDelete(id)
+            .then((author) => (author ? res.status(201).json({ author, message: 'Deleted' }) : res.status(404).json({ message: 'not found' })))
+            .catch((error) => res.status(500).json({ error }));
+}
+
+export default {create, readById, readAll, update, deleteById}
+
---
+++ controllers/BookController.ts
@@ -0,0 +1,62 @@
+import { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import BookModel from '../models/BookModel';
+
+const create = (req: Request, res: Response) => {
+    const { author, title } = req.body;
+
+    const book = new BookModel({
+        _id: new mongoose.Types.ObjectId(),
+        author,
+        title
+    });
+
+    return book
+        .save()
+        .then((book) => res.status(201).json({ book }))
+        .catch((error) => res.status(500).json({ error }));
+};
+
+const readById = (req: Request, res: Response) => {
+    const id = req.params.bookId;
+
+    return BookModel.findById(id)
+        .populate('author')
+        .then((book) => (book ? res.status(200).json({ book }) : res.status(404).json({ message: 'not found' })))
+        .catch((error) => res.status(500).json({ error }));
+};
+
+const readAll = (req: Request, res: Response) => {
+    return BookModel.find()
+        .then((books) => res.status(200).json({ books }))
+        .catch((error) => res.status(500).json({ error }));
+};
+
+const update = (req: Request, res: Response) => {
+    const id = req.params.bookId;
+
+    return BookModel.findById(id)
+        .then((book) => {
+            if (book) {
+                book.set(req.body);
+
+                return book
+                    .save()
+                    .then((book) => res.status(201).json({ book }))
+                    .catch((error) => res.status(500).json({ error }));
+            } else {
+                return res.status(404).json({ message: 'not found' });
+            }
+        })
+        .catch((error) => res.status(500).json({ error }));
+};
+
+const deleteById = (req: Request, res: Response) => {
+    const id = req.params.bookId;
+
+    return BookModel.findByIdAndDelete(id)
+        .then((book) => (book ? res.status(201).json({ book, message: 'Deleted' }) : res.status(404).json({ message: 'not found' })))
+        .catch((error) => res.status(500).json({ error }));
+};
+
+export default { create, readById, readAll, update, deleteById };
